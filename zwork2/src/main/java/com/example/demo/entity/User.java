ea2b9dce29c19c48f53442209614ff1abc51ad22
skoji811 <s.koji811@gmail.com>
2021-07-17 17:23:58 +0900

    first commit

---
+++ zwork2/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,13 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+	
+	private int id;
+	private String name;
+	private String address;
+	private String tell;
+	
+}
