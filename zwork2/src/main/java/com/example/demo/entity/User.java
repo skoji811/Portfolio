0ea762e67d2f6e478d5e568f9cd4384836445464
skoji811 <s.koji811@gmail.com>
2021-07-17 17:29:14 +0900

    f

--- zwork2/src/main/java/com/example/demo/entity/User.java
+++ zwork2/src/main/java/com/example/demo/entity/User.java
@@ -1,13 +1,33 @@
 package com.example.demo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@Table(name= "user")
+
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
 	private int id;
+	
+	@Column
 	private String name;
+	
+	@Column
 	private String address;
+	@Column
 	private String tell;
 	
 }
