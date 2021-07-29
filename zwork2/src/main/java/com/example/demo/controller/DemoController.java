c1b25d6420253bc932017be64ee8179e670cd4c2
skoji811 <s.koji811@gmail.com>
2021-07-30 00:53:53 +0900

    修正

--- zwork2/src/main/java/com/example/demo/controller/DemoController.java
+++ zwork2/src/main/java/com/example/demo/controller/DemoController.java
@@ -3,10 +3,10 @@ package com.example.demo.controller;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
