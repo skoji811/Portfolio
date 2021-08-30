d4d00ba3806dfc9a5d4e33f11e61cbef4aae209a
skoji811 <s.koji811@gmail.com>
2021-08-30 17:35:27 +0900

    first commit

--- zwork3/src/main/java/com/example/demo/entity/User.java
+++ zwork3/src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,24 @@
 package com.example.demo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
 public class User {
+	@Id
+	@Column
+	private String username;
+	@Column
+	private String encodedPassword;
+	
+
 
 }
