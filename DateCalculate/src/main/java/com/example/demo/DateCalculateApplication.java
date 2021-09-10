6acc49c66d6abebd6044c42f94f1717d8505f234
skoji811 <s.koji811@gmail.com>
2021-09-10 18:20:29 +0900

    20210910

---
+++ DateCalculate/src/main/java/com/example/demo/DateCalculateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateCalculateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateCalculateApplication.class, args);
+	}
+
+}
