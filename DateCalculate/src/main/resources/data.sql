a53fdc97fa5b768b6751530a1195dbba97d513f0
skoji811 <s.koji811@gmail.com>
2021-09-10 18:18:44 +0900

    20210910 MySQL情報

---
+++ DateCalculate/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+
+INSERT IGNORE INTO DateCalc (id, name, plusyear, plusmonth, plusday) VALUES (1, 'A', 1, 1, 1);
+INSERT IGNORE INTO DateCalc (id, name, plusyear, plusmonth, plusday) VALUES (2, 'B', 2, 2, 2);
+INSERT IGNORE INTO DateCalc (id, name, plusyear, plusmonth, plusday) VALUES (3, 'C', 3, 3, 3);
---
+++ DateCalculate/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS DateCalc (
+    id INT(50) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    plusyear INT(50),
+    plusmonth INT(50),
+    plusday INT(50)
+    )
+    AUTO_INCREMENT = 1;
